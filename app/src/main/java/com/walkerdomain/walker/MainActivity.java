5a23ee4ed197d542427cfa0dffaad43adb066a4d
root <root@localhost.localdomain>
2018-12-02 08:18:30 +0545

    MEssage

--- app/src/main/java/com/walkerdomain/walker/MainActivity.java
+++ app/src/main/java/com/walkerdomain/walker/MainActivity.java
@@ -1,13 +1,28 @@
 package com.walkerdomain.walker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends FragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onCreate(final Bundle b) {
+        super.onCreate(b);
+        startService(new Intent(this, SensorListener.class));\
+        if(b ==null )
+        {
+            Fragment newFragment= new Fragment_Overview();
+            FragmentTransaction transaction= getSupportFragmentManager().beginTransaction();
+
+            transaction.replace(android.R.id.content,newFragment);
+            transaction.commit();
+        }
+      setContentView(R.layout.activity_main);
     }
-}
+};
+
---
+++ app/src/main/java/com/walkerdomain/walker/SensorListener.java
@@ -0,0 +1,9 @@
+package com.walkerdomain.walker;
+
+import android.app.Service;
+import android.hardware.SensorEventListener;
+
+public class SensorListener extends Service implements SensorEventListener {
+
+
+}
